8be97b6ee32a4bd1ef8f602488fe4e6eb7765608
Shivam Gupta <shivamg@nova.tech>
2021-01-02 06:09:38 +0530

    init commit

---
+++ src/main/java/demo/spring/secuirty/Spring_JWT_15/SpringJwt15Application.java
@@ -0,0 +1,13 @@
+package demo.spring.secuirty.Spring_JWT_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwt15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwt15Application.class, args);
+	}
+
+}
---
+++ src/test/java/demo/spring/secuirty/Spring_JWT_15/SpringJwt15ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.spring.secuirty.Spring_JWT_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwt15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
